04e5f3afb5ae6b7915477b2a8e85bb1df9d83dcc
pokiebob <ccursetjee@gmail.com>
2020-10-29 23:23:37 -0400

    Point class

---
+++ Point.java
@@ -0,0 +1,29 @@
+public class Point{
+  private double x,y;
+
+  //construct a point given coordinates
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+
+  //construct a point given a point  (redundant , just provides flexibility)
+  public Point(Point p){
+    x= p.x;  //private access in the same file is allowed!
+    y= p.y;
+  }
+
+  public double getX(){
+    return x;
+  }
+
+  public double getY(){
+    return y;
+  }
+
+  public static void main(String[] args) {
+    // Point pnt = new Point(0, 0);
+    // System.out.println(pnt.getX());
+  }
+
+}
